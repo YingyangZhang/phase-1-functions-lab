74c95f05c41bd60f95a0f9b64af7a16436ff4126
Yingyang Zhang <yingyang.zhang95@gmail.com>
2022-08-29 20:36:34 -0400

    commit tree 6be85913797c95c655a660ff381f7810fa709941

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(myValue){
+    return Math.abs(myValue - 42);
+}
+
+function distanceFromHqInFeet(myValue){
+    return distanceFromHqInBlocks(myValue) * 264;
+}
+
+function distanceTravelledInFeet(start, destination){
+    return Math.abs((destination - start) * 264);
+}
+
+function calculatesFarePrice(start, destination){
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance < 400){
+        return 0;
+    } else if (distance >= 400 && distance <= 2000){
+        //
+        return 0.02 * (distance - 400);
+    } else if (distance > 2000 && distance < 2500){
+        return 25;
+    } else{
+        return 'cannot travel that far';
+    }
+}
